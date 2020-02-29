f4eedc82fd3873c260b31715b2d275537ad93612
earthctzn <earthctzn1@gmail.com>
2020-02-29 15:19:16 -0500

    Connects Login form to backend and successfuly loggs users in.

--- src/components/loginInput.js
+++ src/components/loginInput.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { LoginCard } from './ComponentStyles'
+import { ErrorComponent } from './ErrorComponent'
+import { Redirect } from 'react-router-dom'
 
 class LoginInput extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        shouldRedirect: false
     }
+
+    componentDidUpdate(prevProps) {
+        if(this.props.user && !prevProps.user) {
+           this.setState({
+               shouldRedirect: true
+           })
+        }
+    }
+
+    renderErrors = () => {
+        return this.props.errors.map(error => <ErrorComponent key={error} error={error} />)
+     } 
 
     handleInputChange = event => {
         this.setState({
@@ -15,15 +31,51 @@ class LoginInput extends Component {
 
     handleOnSubmit = event => {
         event.preventDefault()
-        // LoginUser(this.state)
+        this.LoginUser(this.state)
         this.setState({
             email: '',
             password: ''
-        })        
+        })
+        this.props.clearErrors()        
     }
 
+    LoginUser = async (user) => {
+
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json')
+        headers.append('X-CSRF-Token', document.cookie.split('=')[1])
+        const formData = { user: {
+            email: user.email,
+            password: user.password
+        }};
+        
+        const options = {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(formData)
+        };
+    
+        try{
+            const request = new Request('http://localhost:3000/api/v1/login', options)
+            const response = await fetch(request);
+            const userObj = await response.json()
+            if (userObj.errors) {
+                this.props.setErrors(userObj)
+            }else {
+                console.log(userObj)
+                this.props.setUser(userObj)
+            }
+        } catch(data) {
+            console.log(data)
+        };
+    
+    };
+
     render() {
-        return (
+        return this.state.shouldRedirect ? 
+         (<Redirect to="/home" /> ) 
+         : 
+         (
             <LoginCard >
                 <h3>Login To See all breweries!</h3>
                 <form onSubmit={this.handleOnSubmit}>
@@ -37,7 +89,7 @@ class LoginInput extends Component {
                     </input>
                     <input
                         id="password"
-                        type="text" 
+                        type="password" 
                         placeholder="password"
                         value={this.state.password}
                         onChange={e => this.handleInputChange(e)}
@@ -50,4 +102,25 @@ class LoginInput extends Component {
     }
 }
 
-export default LoginInput
+const mapStateToProps = state => {
+    return {
+        errors: state.users.formErrors,
+        user: state.users.user
+    }
+} 
+
+const mapDispatchToProps = dispatch => {
+    return ({
+        setUser: (userObj) => dispatch({
+            type: 'SET_USER', payload: userObj
+        }),
+        setErrors: (errObj) => dispatch({
+            type: 'ADD_ERRORS', payload: errObj
+        }),
+        clearErrors: () => dispatch({
+            type: 'CLEAR_ERRORS'
+        })
+    })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginInput)
