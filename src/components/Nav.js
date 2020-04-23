1827f983c2e78fa02f2165ff3d1d0c4e02c8d97d
earthctzn <earthctzn1@gmail.com>
2020-04-23 02:13:30 -0400

    adds fbUser action to loginActions and updates nav to use it.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../app.css'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { fbUser } from './actions/loginActions';
+import { fbUser } from '../actions/loginActions';
 
 
 
